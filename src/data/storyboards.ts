43d137dc52e52438a0ac56c9d07eeef8dfe68997
JanuDev <darkober12@gmail.com>
2024-12-12 07:38:43 -0400

    fix: change background color to dark

---
+++ src/data/storyboards.ts
@@ -0,0 +1,28 @@
+const storyboards = [
+  {
+    title: "STRANGER - thriller",
+    storyboard:
+      "https://video.wixstatic.com/video/a9013c_2ca45b029a21401ba58394b77609bba3/480p/mp4/file.mp4",
+  },
+  {
+    title: "Trolls Rough Storyboard",
+    storyboard: {
+      src: [
+        "https://video.wixstatic.com/video/a9013c_1b88e44f99d849098d14dde9d060db26/480p/mp4/file.mp4",
+      ],
+    },
+  },
+  {
+    title: "Stranger (pilot)",
+    storyboard:
+      "https://video.wixstatic.com/video/a9013c_2ca45b029a21401ba58394b77609bba3/480p/mp4/file.mp4",
+  },
+  {
+    title: "Karma (horror)",
+    storyboard:
+      "https://video.wixstatic.com/video/a9013c_2ca45b029a21401ba58394b77609bba3/480p/mp4/file.mp4",
+  },
+];
+
+
+export default storyboards;
