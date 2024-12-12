8e8695f259df17c0f90b3bfe360f06203ff6dfc7
JanuDev <darkober12@gmail.com>
2024-12-12 10:01:14 -0400

    feat: add background images

---
+++ src/data/backgrounds.ts
@@ -0,0 +1,36 @@
+const backgrounds = [
+  {
+    id: 1,
+    src: "https://pbs.twimg.com/media/FQ5ofTjXwAAE5JS?format=jpg&name=4096x4096",
+  },
+  {
+    id: 2,
+    src: "https://pbs.twimg.com/media/FfOICC2WYAApq1y?format=jpg&name=4096x4096",
+  },
+  {
+    id: 3,
+    src: "https://pbs.twimg.com/media/FfOIQ1TXoActuTs?format=jpg&name=4096x4096",
+  },
+  {
+    id: 4,
+    src: "https://pbs.twimg.com/media/FfOITFzXoAEpCXm?format=jpg&name=4096x4096",
+  },
+  {
+    id: 5,
+    src: "https://pbs.twimg.com/media/FfOIUGRXkAUcGTm?format=jpg&name=4096x4096",
+  },
+  {
+    id: 6,
+    src: "https://pbs.twimg.com/media/FfTPp_IXkAE5-ph?format=jpg&name=4096x4096",
+  },
+  {
+    id: 7,
+    src: "https://pbs.twimg.com/media/FfTPx_oXgAEQpfe?format=jpg&name=4096x4096",
+  },
+  {
+    id: 8,
+    src: "https://pbs.twimg.com/media/FQ5ospzXwAAZpST?format=jpg&name=4096x4096",
+  },
+];
+
+export default backgrounds;
