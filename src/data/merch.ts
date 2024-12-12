a000b31c1cc953c1ef1eae1478273b3073143542
JanuDev <darkober12@gmail.com>
2024-12-12 12:54:58 -0400

    feat: add prints page

---
+++ src/data/merch.ts
@@ -0,0 +1,48 @@
+const merchPrints = [
+  {
+    id: 1,
+    src: "https://static.wixstatic.com/media/a9013c_9ef9864845b84a98b73877c732452853~mv2.png/v1/fill/w_505,h_725,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/a9013c_9ef9864845b84a98b73877c732452853~mv2.png",
+  },
+  {
+    id: 2,
+    src: "https://static.wixstatic.com/media/a9013c_43a007b96d714f099a559738cd08e887~mv2.png/v1/fill/w_509,h_725,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/a9013c_43a007b96d714f099a559738cd08e887~mv2.png",
+  },
+  {
+    id: 3,
+    src: "https://static.wixstatic.com/media/a9013c_558db9e5e854471aa0cf85903279d1b5~mv2.png/v1/fill/w_511,h_725,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/a9013c_558db9e5e854471aa0cf85903279d1b5~mv2.png",
+  },
+  {
+    id: 4,
+    src: "https://static.wixstatic.com/media/a9013c_21dbaac9fdab46d0bcd0f719779715a3~mv2.png/v1/fill/w_750,h_515,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/a9013c_21dbaac9fdab46d0bcd0f719779715a3~mv2.png",
+  },
+  {
+    id: 5,
+    src: "https://static.wixstatic.com/media/a9013c_4cd248e86382414187c5c60c4e2fa8d6~mv2.png/v1/fill/w_771,h_515,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/a9013c_4cd248e86382414187c5c60c4e2fa8d6~mv2.png",
+  },
+  {
+    id: 6,
+    src: "https://static.wixstatic.com/media/a9013c_2f71c9b1775a4ae5bd0905d2e3332512~mv2.png/v1/fill/w_486,h_725,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/a9013c_2f71c9b1775a4ae5bd0905d2e3332512~mv2.png",
+  },
+  {
+    id: 7,
+    src: "https://static.wixstatic.com/media/a9013c_27c4c1e1c5b14723b89ec3616a564d83~mv2.png/v1/fill/w_509,h_725,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/a9013c_27c4c1e1c5b14723b89ec3616a564d83~mv2.png",
+  },
+  {
+    id: 8,
+    src: "https://static.wixstatic.com/media/a9013c_92ae1380e8f546b38594acecfab3a8d3~mv2.png/v1/fill/w_526,h_725,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/a9013c_92ae1380e8f546b38594acecfab3a8d3~mv2.png",
+  },
+  {
+    id: 9,
+    src: "https://static.wixstatic.com/media/a9013c_ee90a11f1a76421d8eed5809dd902678~mv2.png/v1/fill/w_486,h_750,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/a9013c_ee90a11f1a76421d8eed5809dd902678~mv2.png",
+  },
+  {
+    id: 10,
+    src: "https://static.wixstatic.com/media/a9013c_3fe0f49f58ac4b759ae4016047d5fd33~mv2.png/v1/fill/w_526,h_750,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/a9013c_3fe0f49f58ac4b759ae4016047d5fd33~mv2.png",
+  },
+  {
+    id: 11,
+    src: "https://static.wixstatic.com/media/a9013c_c9c6616742cd461e968093f82d1696e8~mv2.png/v1/fill/w_509,h_750,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/a9013c_c9c6616742cd461e968093f82d1696e8~mv2.png",
+  },
+];
+
+export default merchPrints;
