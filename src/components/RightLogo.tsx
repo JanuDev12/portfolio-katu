c55b75e4e83990056e30dbb911a61eeaa8593089
JanuDev <darkober12@gmail.com>
2024-12-15 20:34:49 -0400

    refactor: cleaning code

--- src/components/RightLogo.tsx
+++ src/components/RightLogo.tsx
@@ -33,20 +33,6 @@ function RightLogo() {
         src="https://static.wixstatic.com/media/a9013c_35c12ae789e645d4a8611bd9faa22e8c~mv2.png/v1/fill/w_233,h_289,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/FIRMA%20PODEROSA.png"
         alt=""
       />
-      <ul className="flex flex-col text-sm items-start mt-4 uppercase font-proxima font-semibold tracking-tight">
-        <li className="flex items-center relative">
-          <span className="h-[3px] w-5 bg-neutral-600 absolute top-1/2 -left-6 rounded-lg"></span>
-          <span> Stranger</span>
-        </li>
-        <li className='flex relative'>
-          <span className="h-[3px] w-5 bg-neutral-600 absolute top-1/2 -left-6 rounded-lg"></span>
-          <span> Trolls Rough</span>
-        </li>
-        <li className='relative'>
-          <span className="h-[3px] w-5 bg-neutral-600 absolute top-1/2 -left-6 rounded-lg"></span>
-          <span>Karma</span>
-        </li>
-      </ul>
       <div className='size-10 bg-slate-200 rounded-xl absolute bottom-0 ml-4'>
         
       </div>
